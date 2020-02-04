d6af4fb5e8131b525e992d30e4a5cc904910e0cf
shika-blyat <abdelzighel@gmail.com>
2020-02-04 02:35:59 +0100

    Add ws1 function

--- src/combinator/whitespace.rs
+++ src/combinator/whitespace.rs
@@ -1,4 +1,4 @@
-use crate::{Parser, Remaining};
+use crate::{Error, Parser, ParserError, Remaining};
 
 /// Consume everything char by char until it encounters something which is not a whitespace
 pub fn ws<'a>() -> impl Parser<'a, ()> {
@@ -8,5 +8,12 @@ pub fn ws<'a>() -> impl Parser<'a, ()> {
             Remaining::new(&s.rem[s.rem.len()..s.rem.len()], s.pos + s.rem.len()),
             (),
         )),
+    }
+}
+
+pub fn ws1<'a>() -> impl Parser<'a, ()> {
+    |s: Remaining<'a>| match s.rem.find(|c: char| !c.is_whitespace()) {
+        Some(index) => Ok((Remaining::new(&s.rem[index..], s.pos + index), ())),
+        None => Err(Error::Failure(s, ParserError::new_empty())),
     }
 }
